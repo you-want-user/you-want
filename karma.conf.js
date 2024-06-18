77a8eaa74c86b0326ed18c59cd8306e9fcd4e926
Wilian Huanca <whuanca@okode.com>
2024-06-18 16:13:59 +0200

    Refact name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/appbanner'),
+      dir: require('path').join(__dirname, './coverage/you-want'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('appbanner app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('you-want app is running!');
   });
 });
